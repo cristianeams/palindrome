b6e81cc330f01659afb84d6a8ffdceec2e8ac77b
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-05 21:01:05 +0000

    Fixed palindrome errors

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,18 @@
 function isPalindrome(str) {
-  var noSpaces = str.split(" ").join("");
+  var noSpaces = str.split(" ").join("").toLowerCase();
+  //console.log(noSpaces)
   var mid = Math.floor(noSpaces.length/2);
+  //console.log(mid)
   var last = noSpaces.length - 1;
-
+  //console.log(last)
   for (var i = 0; i < mid; i++) {
-    if (str[i] !== str[last - i]) return false;
+    //console.log('first half:', noSpaces[i], 'second half:', noSpaces[last - i])
+    if (noSpaces[i] !== noSpaces[last - i]) {
+      return false;
+    }
   }
+  return true;
 }
-
 // Test driver code. These should all log true.
 console.log(isPalindrome('p') === true);
 console.log(isPalindrome('foo') === false);
@@ -16,4 +21,3 @@ console.log(isPalindrome('Kayak') === true);
 console.log(isPalindrome('a santa at NASA') === true);
 console.log(isPalindrome('live without evil') === false);
 console.log(isPalindrome('just some random words') === false);
-
